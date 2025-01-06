4953017488b2b262658db0c2db50a3a89083fc12
Collin MacDonald <cmacdonald01@wm.edu>
2025-01-05 19:53:47 -0500

    Move docker user name into env var

--- dev/pilot-deploy.sh
+++ dev/pilot-deploy.sh
@@ -2,9 +2,9 @@
 
 make DEBUG=1 docker.pilot
 make push.docker.pilot
-kubectl set image deployment/istiod -n istio-system discovery=atosh502/pilot:atosh502
+kubectl set image deployment/istiod -n istio-system discovery=$DOCKER_USER/pilot:$DOCKER_USER
 kubectl rollout restart deployment istiod -n istio-system
 
 # make DEBUG=1 docker.pilot && make push.docker.pilot
 
-# make DEBUG=1 docker.pilot && make push.docker.pilot && kubectl set image deployment/istiod -n istio-system discovery=atosh502/pilot:atosh502 && kubectl rollout restart deployment istiod -n istio-system
+# make DEBUG=1 docker.pilot && make push.docker.pilot && kubectl set image deployment/istiod -n istio-system discovery=$DOCKER_USER/pilot:$DOCKER_USER && kubectl rollout restart deployment istiod -n istio-system
