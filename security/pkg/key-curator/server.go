91f47d883d6fa38fded5038090cebed67749b6b8
Aashutosh Poudel <apoudel01@wm.edu>
2025-03-21 12:51:48 -0400

    init new istiod with common pp istiod watch for new registered users from etcd init existing users on istiod at startup

--- security/pkg/key-curator/server.go
+++ security/pkg/key-curator/server.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"strconv"
+	"strings"
 	"time"
 
 	bls "github.com/cloudflare/circl/ecc/bls12381"
@@ -16,7 +18,11 @@ import (
 	pb "istio.io/istio/security/pkg/key-curator/key-curator"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
+	gproto "google.golang.org/protobuf/proto"
 )
+
+const RBE_USER_PREFIX = "rbe-user/"
+const RBE_PP_KEY = "rbe-system/pp"
 
 type RegistrationEvent struct {
 	token     string
@@ -25,6 +31,17 @@ type RegistrationEvent struct {
 	id        int
 	publicKey *bls.G1
 	xi        []*bls.G1
+
+	source string // either api or etcd
+}
+
+// TODO: rename this to something more meaningful
+type UserRequest struct {
+	id     int
+	req    *pb.RegisterRequest
+	source string
+
+	respChan chan *pb.UserOpeningResponse
 }
 
 type KeyCuratorServer struct {
@@ -32,8 +49,11 @@ type KeyCuratorServer struct {
 	kc *rbe.KeyCurator
 	pp *rbe.PublicParams
 
-	history    []*RegistrationEvent
-	EtcdClient *clientv3.Client
+	history       []*RegistrationEvent
+	EtcdClient    *clientv3.Client
+	registeredIds map[int]bool // used to track registered user ids
+
+	registrationQueue chan UserRequest
 
 	// todo: see how authenticators are used
 	Authenticators []security.Authenticator
@@ -50,7 +70,14 @@ func (kcs *KeyCuratorServer) initEtcdWithRetry() {
 		err := kcs.tryConnectToEtcd()
 		if err == nil {
 			log.Infof("[dev] Successfully connected to etcd after %d attempts", attempts)
-			go kcs.watchEtcdKeys() // Start watching in background
+
+			// Restore system params if any exist
+			kcs.restoreSystemParams()
+
+			kcs.listenRegistrationRequests()
+
+			go kcs.watchEtcdKeys()      // Start watching in background
+			go kcs.fetchExistingUsers() // Fetch existing users from etcd
 			return
 		}
 
@@ -64,6 +91,93 @@ func (kcs *KeyCuratorServer) initEtcdWithRetry() {
 		// Sleep with exponential backoff, capped at maxBackoff
 		time.Sleep(backoff)
 		backoff = time.Duration(math.Min(float64(backoff*2), float64(maxBackoff)))
+	}
+}
+
+// if there's existing public params in etcd, restore them
+// if not update the public params in etcd with the current ones
+func (kcs *KeyCuratorServer) restoreSystemParams() {
+	ppRes, err := kcs.EtcdClient.Get(context.Background(), RBE_PP_KEY)
+	if err != nil {
+		log.Errorf("[dev] failed to fetch public params from etcd: %v", err)
+		return
+	}
+
+	log.Infof("[dev] fetched %d public params from etcd", len(ppRes.Kvs))
+
+	if len(ppRes.Kvs) > 0 {
+		log.Infof("[dev] found public params in etcd, restoring them")
+
+		record := ppRes.Kvs[0]
+		ppValue := record.Value
+
+		ppProto := &proto.PublicParams{}
+		err := gproto.Unmarshal([]byte(ppValue), ppProto)
+		if err != nil {
+			log.Errorf("[dev] failed to unmarshal public params from etcd: %v", err)
+			return
+		}
+
+		kcs.pp.FromProto(ppProto)
+		kcs.kc = rbe.NewKeyCurator(kcs.pp) // reinitialize KeyCurator with restored public params
+		log.Infof("[dev] restored public params from etcd")
+	} else {
+		log.Infof("[dev] no public params found in etcd, storing current public params")
+		pp := kcs.pp.ToProto()
+		ppValue, err := gproto.Marshal(pp)
+		if err != nil {
+			log.Errorf("[dev] failed to marshal public params: %v", err)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		_, err = kcs.EtcdClient.Put(ctx, RBE_PP_KEY, string(ppValue))
+		if err != nil {
+			log.Errorf("[dev] failed to store public params in etcd: %v", err)
+			return
+		}
+
+		log.Infof("[dev] stored current public params in etcd")
+	}
+}
+
+func (kcs *KeyCuratorServer) fetchExistingUsers() {
+	getRes, err := kcs.EtcdClient.Get(context.Background(), RBE_USER_PREFIX, clientv3.WithPrefix())
+	if err != nil {
+		log.Errorf("[dev] failed to fetch existing users from etcd: %v", err)
+		return
+	}
+
+	log.Infof("[dev] fetched %d existing users from etcd", len(getRes.Kvs))
+	for _, kv := range getRes.Kvs {
+		value := kv.Value
+
+		req := &pb.RegisterRequest{}
+		err := gproto.Unmarshal([]byte(value), req)
+		if err == nil {
+			id := int(req.Id)
+			userReq := UserRequest{
+				id:       id,
+				req:      req,
+				source:   "etcd",
+				respChan: make(chan *pb.UserOpeningResponse, 1),
+			}
+
+			kcs.registrationQueue <- userReq
+
+			resp := <-userReq.respChan // wait for the response
+			close(userReq.respChan)
+			// ignore the response for now
+			if resp == nil {
+				log.Errorf("[dev] error registering user %d from etcd: %v", id, err)
+			} else {
+				log.Infof("[dev] registered user %d from etcd", id)
+			}
+		} else {
+			log.Infof("[dev] error unmarshalling request for user %d: %v", kv.Key, err)
+		}
 	}
 }
 
@@ -92,7 +206,7 @@ func (kcs *KeyCuratorServer) tryConnectToEtcd() error {
 }
 
 func (kcs *KeyCuratorServer) watchEtcdKeys() {
-	rch := kcs.EtcdClient.Watch(context.Background(), "rbe-users", clientv3.WithPrefix())
+	rch := kcs.EtcdClient.Watch(context.Background(), RBE_USER_PREFIX, clientv3.WithPrefix())
 	log.Infof("[dev] lets print the watch channel itself %+v", rch)
 	for wresp := range rch {
 		if wresp.Canceled {
@@ -102,20 +216,120 @@ func (kcs *KeyCuratorServer) watchEtcdKeys() {
 
 		log.Infof("[dev] etcd watch response: %+v", wresp)
 		for _, ev := range wresp.Events {
-			log.Infof("[dev] %s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+			log.Infof("[dev] type: %s, key: %q\n", ev.Type, ev.Kv.Key)
+
+			if ev.Type == clientv3.EventTypePut {
+				// key is of form: "rbe-user/13610"
+				key := string(ev.Kv.Key)
+				value := ev.Kv.Value
+
+				parts := strings.Split(key, "/")
+				if len(parts) == 2 {
+					id, err := strconv.Atoi(parts[1])
+					if err != nil {
+						log.Warnf("[dev] error parsing user id: %d, %v", id, err)
+						continue
+					}
+
+					_, registered := kcs.registeredIds[id]
+					if registered {
+						log.Infof("[dev] user with id %d is already registered, skipping", id)
+					} else {
+						// finally add user to your id space
+						req := &pb.RegisterRequest{}
+						err := gproto.Unmarshal([]byte(value), req)
+						if err == nil {
+							userReq := UserRequest{
+								id:       id,
+								req:      req,
+								source:   "etcd",
+								respChan: make(chan *pb.UserOpeningResponse, 1),
+							}
+
+							kcs.registrationQueue <- userReq
+
+							resp := <-userReq.respChan // wait for the response
+							close(userReq.respChan)
+							// ignore the response for now
+							if resp == nil {
+								log.Errorf("[dev] error registering user %d from etcd: %v", id, err)
+							} else {
+								log.Infof("[dev] registered user %d from etcd", id)
+							}
+						} else {
+							log.Infof("[dev] error unmarshalling request for user %d: %v", id, err)
+						}
+					}
+				}
+			}
 		}
 	}
+}
+
+func (kcs *KeyCuratorServer) listenRegistrationRequests() {
+	go func() {
+		for request := range kcs.registrationQueue {
+			_, registered := kcs.registeredIds[request.id]
+			if registered {
+				log.Infof("[dev] user with id %d is already registered, skipping", request.id)
+				continue
+			}
+
+			// Process the registration request (one at a time)
+			result, err := kcs.registerUserUtil(request.id, request.req, request.source)
+			if err != nil {
+				log.Errorf("[dev] error processing registration request for user %d: %v", request.id, err)
+				continue
+			}
+
+			// send a response back
+			if request.respChan != nil {
+				request.respChan <- result
+			}
+		}
+	}()
+}
+
+// StoreAtEtcd sends request to etcd server to store the user id and the
+// exact user request
+func (kcs *KeyCuratorServer) StoreAtEtcd(id int, req *pb.RegisterRequest) {
+	if kcs.EtcdClient == nil {
+		log.Warnf("[dev] etcd client is not initialized, cannot store user")
+		return
+	}
+
+	key := fmt.Sprintf("%s%d", RBE_USER_PREFIX, id)
+	value, err := gproto.Marshal(req)
+	if err != nil {
+		log.Errorf("[dev] failed to marshal request for user %d: %v", id, err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = kcs.EtcdClient.Put(ctx, key, string(value))
+	if err != nil {
+		log.Errorf("[dev] failed to store user in etcd: %v", err)
+		return
+	}
+
+	log.Infof("[dev] stored user %d in etcd", id)
 }
 
 func NewKeyCuratorServer(maxUsers int) *KeyCuratorServer {
 	pp := rbe.NewPublicParams(maxUsers)
 	kc := rbe.NewKeyCurator(pp)
 	history := make([]*RegistrationEvent, 0)
+	registeredIds := make(map[int]bool)
 
 	kcServer := &KeyCuratorServer{
-		pp:      pp,
-		kc:      kc,
-		history: history,
+		pp:            pp,
+		kc:            kc,
+		history:       history,
+		registeredIds: registeredIds,
+
+		registrationQueue: make(chan UserRequest, 100),
 	}
 
 	go kcServer.initEtcdWithRetry()
@@ -127,8 +341,11 @@ func (kcs *KeyCuratorServer) FetchPublicParams(_ context.Context, in *emptypb.Em
 	return &pb.PublicParamsResponse{Pp: kcs.pp.ToProto()}, nil
 }
 
-func (kcs *KeyCuratorServer) addToHistory(token string, ip string, port string, id int, publicKey *bls.G1, xi []*bls.G1) {
-	kcs.history = append(kcs.history, &RegistrationEvent{token, ip, port, id, publicKey, xi})
+// how does history change when multiple istiod instances are running?
+func (kcs *KeyCuratorServer) addToHistory(token string, ip string, port string,
+	id int, publicKey *bls.G1, xi []*bls.G1, source string) {
+	kcs.history = append(kcs.history,
+		&RegistrationEvent{token, ip, port, id, publicKey, xi, source})
 }
 
 // fetches updates for all users
@@ -194,15 +411,10 @@ func (kcs *KeyCuratorServer) FetchUpdate(_ context.Context, in *pb.UpdateRequest
 	return &pb.UserOpeningResponse{Opening: opening, Commitments: commitments}, nil
 }
 
-func (kcs *KeyCuratorServer) RegisterUser(_ context.Context, in *pb.RegisterRequest) (*pb.UserOpeningResponse, error) {
-	log.Infof("[dev] received register request for user with id: %d", in.GetId())
-	// log.Infof("[dev] the register request %v", in)
-
-	id := int(in.GetId())
+func (kcs *KeyCuratorServer) registerUserUtil(id int, in *pb.RegisterRequest,
+	source string) (*pb.UserOpeningResponse, error) {
 	publicKey := new(bls.G1)
 	publicKey.SetBytes(in.GetPublicKey().GetPoint())
-
-	// log.Infof("[dev] public key %v", publicKey)
 
 	xi := make([]*bls.G1, len(in.GetXi()))
 	for i, v := range in.GetXi() {
@@ -215,10 +427,8 @@ func (kcs *KeyCuratorServer) RegisterUser(_ context.Context, in *pb.RegisterRequ
 		}
 	}
 
-	// log.Infof("[dev] xi values %v", xi)
-
 	kcs.kc.RegisterUser(id, publicKey, xi)
-	kcs.addToHistory(in.Token, in.Ip, in.Port, int(in.Id), publicKey, xi)
+	kcs.addToHistory(in.Token, in.Ip, in.Port, int(in.Id), publicKey, xi, source)
 
 	opening := []*proto.G1{}
 	for _, v := range kcs.kc.UserOpenings[id] {
@@ -230,7 +440,50 @@ func (kcs *KeyCuratorServer) RegisterUser(_ context.Context, in *pb.RegisterRequ
 		commitments = append(commitments, &proto.G1{Point: v.Bytes()})
 	}
 
+	kcs.registeredIds[id] = true
+	if source == "api" {
+		// only send to etcd if registering a new user via API
+		// this means only this instance of istiod received this request
+		// so we need to sent it to etcd so that other instances can pick it up
+		kcs.StoreAtEtcd(id, in)
+	}
+
 	return &pb.UserOpeningResponse{Opening: opening, Commitments: commitments}, nil
+}
+
+func (kcs *KeyCuratorServer) RegisterUser(_ context.Context, in *pb.RegisterRequest) (*pb.UserOpeningResponse, error) {
+	log.Infof("[dev] received register request for user with id: %d", in.GetId())
+
+	id := int(in.GetId())
+	// rethink the check for registered user ids
+	_, registered := kcs.registeredIds[id]
+	if registered {
+		log.Warnf("[dev] user with id %d is already registered", id)
+		return &pb.UserOpeningResponse{
+			Opening:     []*proto.G1{},
+			Commitments: []*proto.G1{},
+		}, fmt.Errorf("user with id %d is already registered", id)
+	}
+
+	userReq := UserRequest{
+		id:       id,
+		req:      in,
+		source:   "api",
+		respChan: make(chan *pb.UserOpeningResponse, 1),
+	}
+
+	kcs.registrationQueue <- userReq
+
+	userOpeningResp := <-userReq.respChan // wait for the response
+	close(userReq.respChan)
+	if userOpeningResp == nil {
+		errMsg := fmt.Errorf("[dev] error registering user %d from api", id)
+		log.Errorf(errMsg.Error())
+		return nil, errMsg
+	}
+	log.Infof("[dev] registered user %d from api", id)
+
+	return userOpeningResp, nil
 }
 
 // Register registers a GRPC server on the specified port.
