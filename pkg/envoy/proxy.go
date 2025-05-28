730eaec5c54cd6bb5ad98d483855cded227f02bf
Aashutosh Poudel <apoudel01@wm.edu>
2025-05-28 17:13:23 -0400

    Revert "disable logging"
    This reverts commit f751e7a43a4be71a720a950fea757da94942e874.

--- pkg/envoy/proxy.go
+++ pkg/envoy/proxy.go
@@ -61,9 +61,8 @@ type ProxyConfig struct {
 func NewProxy(cfg ProxyConfig) Proxy {
 	// inject tracing flag for higher levels
 	var args []string
-	// logLevel, componentLogs := splitComponentLog("debug,misc:info")
-	// logLevel, componentLogs := splitComponentLog("debug,misc:error")
-	logLevel, componentLogs := splitComponentLog(cfg.LogLevel)
+	// logLevel, componentLogs := splitComponentLog(cfg.LogLevel)
+	logLevel, componentLogs := splitComponentLog("debug,misc:info")
 	if logLevel != "" {
 		args = append(args, "-l", logLevel)
 	}
--- pkg/log/options.go
+++ pkg/log/options.go
@@ -107,10 +107,9 @@ type Options struct {
 // DefaultOptions returns a new set of options, initialized to the defaults
 func DefaultOptions() *Options {
 	return &Options{
-		OutputPaths:      []string{defaultOutputPath},
-		ErrorOutputPaths: []string{defaultErrorOutputPath},
-		// defaultOutputLevels:  "default:debug,grpc:none,ca:debug",
-		defaultOutputLevels:  "default:info,grpc:none",
+		OutputPaths:          []string{defaultOutputPath},
+		ErrorOutputPaths:     []string{defaultErrorOutputPath},
+		defaultOutputLevels:  "default:debug,grpc:none,ca:debug",
 		stackTraceLevels:     DefaultScopeName + ":" + levelToString[defaultStackTraceLevel],
 		logGRPC:              false,
 		useStackdriverFormat: false,
