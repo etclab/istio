f751e7a43a4be71a720a950fea757da94942e874
Aashutosh Poudel <apoudel01@wm.edu>
2025-05-28 17:06:50 -0400

    disable logging

--- pilot/cmd/pilot-agent/options/agent_proxy.go
+++ pilot/cmd/pilot-agent/options/agent_proxy.go
@@ -56,5 +56,5 @@ func NewProxyArgs() ProxyArgs {
 func (node *ProxyArgs) applyDefaults() {
 	node.PodName = PodNameVar.Get()
 	node.PodNamespace = PodNamespaceVar.Get()
-	node.ProxyLogLevel = "debug"
+	// node.ProxyLogLevel = "debug"
 }
